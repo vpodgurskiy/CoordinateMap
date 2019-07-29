56e2c8a5b26f3eb90fdda11f9b6ea0f85f7ce783
vpodgurskiy <vpodgurskiy@gmail.com>
2019-07-29 19:15:51 +0300

    fix

--- bpmnProcess/src/main/java/org/comunda/bpmn/addressMap/AddressMapping.java
+++ bpmnProcess/src/main/java/org/comunda/bpmn/addressMap/AddressMapping.java
@@ -1,28 +1,11 @@
 package org.comunda.bpmn.addressMap;
 
-public class AddressMapping {
-    //private final static Logger LOGGER = Logger.getLogger(AddressMapping.class.getName());
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
 
+@ProcessApplication("Address Mapping")
+public class AddressMapping extends ServletProcessApplication {
     public static void main(String[] args) {
-//        ExternalTaskClient client = ExternalTaskClient.create()
-//                .baseUrl("http://localhost:8080/engine-rest")
-//                .asyncResponseTimeout(10000) // long polling timeout
-//                .build();
-//
-//        // subscribe to an external task topic as specified in the process
-//        client.subscribe("addressMap")
-//                .lockDuration(1000) // the default lock duration is 20 seconds, but you can override this
-//                .handler((externalTask, externalTaskService) -> {
-//                    // Put your business logic here
-//
-//                    // Get a process variable
-//                    String item = (String) externalTask.getVariable("item");
-//                    Long amount = (Long) externalTask.getVariable("amount");
-////                    LOGGER.info("Charging credit card with an amount of '" + amount + "'€ for the item '" + item + "'...");
-//
-//                    // Complete the task
-//                    externalTaskService.complete(externalTask);
-//                })
-//                .open();
+
     }
 }
