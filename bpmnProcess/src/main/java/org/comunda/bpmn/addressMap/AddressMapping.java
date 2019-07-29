4ca566eaa365a5b727ebe72a3c770d869cf17127
vpodgurskiy <vpodgurskiy@gmail.com>
2019-07-29 18:50:27 +0300

    add bpmn

--- bpmnProcess/src/main/java/org/comunda/bpmn/addressMap/AddressMapping.java
+++ bpmnProcess/src/main/java/org/comunda/bpmn/addressMap/AddressMapping.java
@@ -1,4 +1,28 @@
 package org.comunda.bpmn.addressMap;
 
 public class AddressMapping {
+    //private final static Logger LOGGER = Logger.getLogger(AddressMapping.class.getName());
+
+    public static void main(String[] args) {
+//        ExternalTaskClient client = ExternalTaskClient.create()
+//                .baseUrl("http://localhost:8080/engine-rest")
+//                .asyncResponseTimeout(10000) // long polling timeout
+//                .build();
+//
+//        // subscribe to an external task topic as specified in the process
+//        client.subscribe("addressMap")
+//                .lockDuration(1000) // the default lock duration is 20 seconds, but you can override this
+//                .handler((externalTask, externalTaskService) -> {
+//                    // Put your business logic here
+//
+//                    // Get a process variable
+//                    String item = (String) externalTask.getVariable("item");
+//                    Long amount = (Long) externalTask.getVariable("amount");
+////                    LOGGER.info("Charging credit card with an amount of '" + amount + "'€ for the item '" + item + "'...");
+//
+//                    // Complete the task
+//                    externalTaskService.complete(externalTask);
+//                })
+//                .open();
+    }
 }
